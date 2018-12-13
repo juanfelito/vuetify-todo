3cecc2c6677c70aadd7a73f6f2a4b29401527f4b
Juan Balcero <juan.balcero@geniussportsmedia.com>
2018-12-13 14:18:24 -0500

    Lots of progress

---
+++ src/components/Popup.vue
@@ -0,0 +1,45 @@
+<template>
+  <v-dialog max-width="600px">
+    <v-btn flat slot="activator" class="success">Add new project</v-btn>
+    <v-card>
+      <v-card-title>
+        <h2>Add a new project</h2>
+      </v-card-title>
+      <v-card-text>
+        <v-form class="px-3">
+          <v-text-field label="Title" v-model="title" prepend-icon="folder"></v-text-field>
+          <v-textarea label="Information" v-model="content" prepend-icon="edit"></v-textarea>
+          <v-menu>
+            <v-text-field :value="formattedDate" slot="activator" label="Due date" prepend-icon="date_range"></v-text-field>
+            <v-date-picker v-model="due"></v-date-picker> 
+          </v-menu>
+          <v-spacer></v-spacer>
+          <v-btn flat class="success mx-0 mt-3" @click="submit()">Add project</v-btn>
+        </v-form>
+      </v-card-text>
+    </v-card>
+  </v-dialog>
+</template>
+
+<script>
+import format from 'date-fns/format';
+export default {
+  data() {
+    return {
+      title: '',
+      content: '',
+      due: null
+    }
+  },
+  methods: {
+    submit() {
+      console.log(this.title + ' ' + this.content);      
+    }
+  },
+  computed: {
+    formattedDate() {
+      return this.due ? format(this.due, 'Do MMM YYYY') : '';
+    }
+  }
+}
+</script>
--- src/components/navbar.vue
+++ src/components/navbar.vue
@@ -7,6 +7,17 @@
         <span>Vuetify</span>
       </v-toolbar-title>
       <v-spacer></v-spacer>
+      <v-menu offset-y>
+        <v-btn flat slot="activator" color="grey">
+          <v-icon left >expand_more</v-icon>
+          <span>menu</span>
+        </v-btn>
+        <v-list>
+          <v-list-tile v-for="link in links" :key="link.text" router :to="link.route">
+            <v-list-tile-title>{{link.text}}</v-list-tile-title>
+          </v-list-tile>
+        </v-list>
+      </v-menu>
       <v-btn flat color="grey">
         <span>Sign Out</span>
         <v-icon right>exit_to_app</v-icon>
@@ -14,6 +25,17 @@
     </v-toolbar>
 
     <v-navigation-drawer v-model="drawer" app class="primary">
+      <v-layout column align-center>
+        <v-flex class="mt-5">
+          <v-avatar size="100">
+            <img src="/avatar-1.png">
+          </v-avatar>
+          <p class="white--text subheading mt-1">The Net Ninja</p>
+        </v-flex>
+        <v-flex class="mt-4 mb-3">
+          <Popup/>
+        </v-flex>
+      </v-layout>
       <v-list>
         <v-list-tile v-for="(link, index) in links" :key="index" router :to="link.route">
           <v-list-tile-action>
@@ -31,16 +53,20 @@
 </template>
 
 <script>
+import Popup from './Popup';
 export default {
-    data() {
-        return {
-            drawer: false,
-            links: [
-                {icon: "dashboard", text: "Dashboard", route: "/"},
-                {icon: "folder", text: "My Projects", route: "/projects"},
-                {icon: "person", text: "Team", route: "/team"}
-            ]
-        }
+  components: {
+    Popup
+  },
+  data() {
+    return {
+      drawer: false,
+      links: [
+        {icon: "dashboard", text: "Dashboard", route: "/"},
+        {icon: "folder", text: "My Projects", route: "/projects"},
+        {icon: "person", text: "Team", route: "/team"}
+      ]
     }
+  }
 }
 </script>
